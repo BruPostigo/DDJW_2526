0ff6abc8cbf18992f762c3ff0dbb1bdd8c96555a
Bru Postigo <u6105606@campus.udg.edu>
2026-03-09 17:10:47 +0100

    Modificat el boto jugar per demanar i mostrar l'alies

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 addEventListener('load', function() {
     document.getElementById('play').addEventListener('click', 
     function(){
+        let aliesJugador = prompt("Introdueix el teu àlies per jugar:");
+        console.log(aliesJugador);
         window.location.assign("./html/game.html");
     });
 
