b427826ea43b4675826d6a861f2871466a169a8f
Bru Postigo <u6105606@campus.udg.edu>
2026-04-26 12:38:37 +0200

    Issue 1 resolta: Afegit javascript per canviar de pantalles al menu

--- js/main.js
+++ js/main.js
@@ -1,40 +1,28 @@
-addEventListener('load', function() {
-    document.getElementById('play').addEventListener('click', 
-    function(){
-        sessionStorage.removeItem('load');
-        window.location.assign("./html/game.html");
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const pantallaMenu = document.getElementById('pantalla-menu');
+    const pantallaJoc = document.getElementById('pantalla-joc');
+    
+    const btnJugar = document.getElementById('btn-jugar');
+    const btnOpcions = document.getElementById('btn-opcions');
+    const btnPuntuacions = document.getElementById('btn-puntuacions');
+    const btnCarregar = document.getElementById('btn-carregar');
+
+    btnJugar.addEventListener('click', () => {
+        pantallaMenu.style.display = 'none';
+        pantallaJoc.style.display = 'block';
+        console.log("Iniciant la partida... Carregant Canvas.");
     });
 
-    document.getElementById('options').addEventListener('click', 
-    function(){
-        window.location.assign("./html/options.html");
+    btnOpcions.addEventListener('click', () => {
+        alert("Opcions del joc (A la Issue 4 i 5 ho programarem)");
     });
 
-    document.getElementById('saves').addEventListener('click', 
-    function(){
-        let to_load = localStorage.save;
-        fetch('../php/load.php', {
-            method: "POST",
-            body: JSON.stringify({}),
-            headers: {"Content-type": "application/json; charset=UTF-8"}
-        })
-        .then(response => response.json())
-        .then(json => to_load = (!json.error)?JSON.stringify(json.save): localStorage.save)
-        .catch (err => {
-            console.error(err);
-            console.warn("La partida s'intentarà carregar de local");
-        });
-
-        if (!to_load) {
-            alert("No hi ha cap partida a carregar");
-            return;
-        }
-        sessionStorage.load = to_load;
-        window.location.assign("./html/game.html");
+    btnPuntuacions.addEventListener('click', () => {
+        alert("Pantalla de puntuacions");
     });
 
-    document.getElementById('exit').addEventListener('click', 
-    function(){
-        console.warn("No es pot sortir!");
+    btnCarregar.addEventListener('click', () => {
+        alert("Carregant partida des de localStorage...");
     });
 });
